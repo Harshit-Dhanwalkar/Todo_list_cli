356f536dad67bd7ddb4cd4452b77a596ae2be5ea
Harshit-Dhanwalkar <harshitpd1729@gmail.com>
2024-12-16 21:08:34 +0530

    Add UI

--- src/main.rs
+++ src/main.rs
@@ -4,32 +4,84 @@ use std::cmp::*;
 const REGULAR_PAIR: i16 = 0;
 const HIGHLIGHT_PAIR: i16 = 1;
 
+#[derive(Default)]
+struct Ui {
+    list_curr: Option<Id>,
+}
+
+impl Ui {
+    fn begin(&mut self) {
+        // TODO:
+    }
+
+    fn begin_list(&mut self, id: Id) {
+        assert!(self.list_curr.is_none(), "Nested list are not allowed!");
+        self.list_curr = Some(id);
+    }
+
+    fn list_element(&mut self, label: &str, id: Id) {
+            // let pair = {
+            //     if todo_curr == index {
+            //         HIGHLIGHT_PAIR
+            //     } else {
+            //         REGULAR_PAIR
+            //     }
+            // };
+            //
+            // attron(COLOR_PAIR(pair));
+            // mv(index as i32, 1);
+            // let _ = addstr(todo);
+            // attroff(COLOR_PAIR(pair));
+        // TODO:
+    }
+
+    fn label(&mut self, text: &str){}
+
+    fn end_list(&mut self) {
+        // TODO:
+    }
+    
+    fn end_list() {
+        // TODO:
+    }
+}
+
 fn main() {
     initscr();
+    noecho();
+    curs_set(CURSOR_VISIBILITY::CURSOR_INVISIBILE);
 
     start_color();
     init_pair(REGULAR_PAIR, COLOR_WHITE, COLOR_BLACK);
     init_pair(HIGHLIGHT_PAIR, COLOR_BLACK, COLOR_WHITE);
 
     let mut quit = false;
-    let todos = vec!["write", "sleep", "coffee"];
-    let mut todo_curr: usize = 0;
+    let todos: Vec<String> = vec![
+        "write".to_string, "sleep".to_string, "coffee".to_string];
+    let mut todo_curr: usize = 1;
+    let dones = Vec::<String>::new();
+    let mut done_current: usize = 0;
 
+    let mut ui = Ui::default();
     while !quit {
  //       clear(); // Clear the screen before redrawing
+        ui.begin();
+        {
+            ui.begin_list(todo_curr);
+            for (index, todo) in todos.iter().enumerate() {
+                ui.list_element(todo, index);
+            }
+            ui.end_list();
 
-        for (row, todo) in todos.iter().enumerate() {
-            let pair = if todo_curr == row {
-                HIGHLIGHT_PAIR
-            } else {
-                REGULAR_PAIR
-            };
+            ui.label("------------------------------");
 
-            attron(COLOR_PAIR(pair));
-            mv(row as i32, 0);
-            let _ = addstr(todo);
-            attroff(COLOR_PAIR(pair));
+            ui.begin_list();
+            for (index, done) in dones.iter().enumerate() {
+                ui.list_element(done, index);
+            }
+            ui.end_list();
         }
+        ui.end();
 
         refresh();
 
@@ -41,7 +93,7 @@ fn main() {
                     todo_curr += 1;
                 }
             }
-            'k' => todo_curr = min(todo_curr + 1, todos.len()),
+            'k' => todo_curr = min(todo_curr + 1, todos.len() -1),
             _ => {}
         }
     }
